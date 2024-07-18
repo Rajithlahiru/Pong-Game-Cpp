8d6fdb390a1a99405521d661b0e6caa4da693eb2
Rajithlahiru <rajithlahiru7@gmail.com>
2024-07-18 12:35:19 +0530

    finished

--- Pong Game/Pong Game.cpp
+++ Pong Game/Pong Game.cpp
@@ -3,6 +3,14 @@
 
 using namespace std;
 
+
+int player_score = 0;
+int cpu_score = 0;
+Color Green = Color{38, 185, 154, 255};
+Color DarkGreen = Color{20, 160, 133, 255};
+Color LightGreen = Color{129, 185, 154, 255};
+Color Yellow = Color{243, 213, 91, 255};
+
 class Ball {
 public:
     float x, y;
@@ -10,7 +18,7 @@ public:
     int radius;
 
     void Draw() {
-        DrawCircle(x, y, radius, WHITE);
+        DrawCircle(x, y, radius, Yellow);
 
     };
 
@@ -21,21 +29,44 @@ public:
         if(y + radius >= GetScreenHeight() || y - radius <= 0) {
 			speed_y *= -1;
 		}
-        if (x + radius >= GetScreenWidth() || x - radius <= 0) {
-            speed_x *= -1;
+        if (x + radius >= GetScreenWidth()) // cpu wins
+        {
+            player_score++;
+            ResetBall();
+        }
+        if(x - radius <= 0) {
+            cpu_score++;
+            ResetBall();
 		}
     }
+    void ResetBall() {
+		x = GetScreenWidth() / 2;
+		y = GetScreenHeight() / 2;
+		
+        int speed_choice[2] = {1, -1};
+        speed_x *= speed_choice[GetRandomValue(0,1)];
+        speed_y *= speed_choice[GetRandomValue(0,1)];
+	}
 
 };
 
 class Paddle {
+protected:
+    void LimitMovement() {
+        if (y <= 0) {
+            y = 0;
+        }
+        if (y + height >= GetScreenHeight()) {
+            y = GetScreenHeight() - height;
+        }
+	}
 public:
 	float x, y;
 	int speed;
 	float width, height;
 
 	void Draw() {
-		DrawRectangle(x, y, width, height, WHITE);
+        DrawRectangleRounded(Rectangle{x,y,width,height}, 0.8, 0, WHITE);
 	}
     void Update() {
 		if (IsKeyDown(KEY_UP)) {
@@ -44,12 +75,7 @@ public:
 		if (IsKeyDown(KEY_DOWN)) {
 			y += speed;
 		}
-        if (y <= 0) {
-			y = 0;
-		}
-        if (y + height >= GetScreenHeight()) {
-			y = GetScreenHeight() - height;
-		}
+		LimitMovement();
 	}
 };
 
@@ -62,6 +88,8 @@ public:
 		}
         if (y + height / 2 <= ball_y) {
             y += speed;
+        }
+        LimitMovement();
     }
 };
 
@@ -104,12 +132,25 @@ int main()
         player.Update();
         cpu.update(ball.y);
 
+        // Collision
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height})) {
+			ball.speed_x *= -1;
+		}
+        if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height})) {
+            ball.speed_x *= -1;
+        }
+
+
         // Drawaing
-        ClearBackground(BLACK);
+        ClearBackground(DarkGreen);
+        DrawRectangle(screenWidth / 2 , 0 , screenHeight / 2, screenHeight, Green); // middle line
+        DrawCircle(screenWidth / 2, screenHeight / 2, 150, LightGreen); // middle circle
         DrawLine(screenWidth/2,0,screenWidth/2,screenHeight,WHITE);
         ball.Draw();
         cpu.Draw();
         player.Draw();
+        DrawText(TextFormat("%i", player_score), screenWidth / 2 - 100, 50, 50, WHITE);
+        DrawText(TextFormat("%i", cpu_score), screenWidth / 2 + 50, 50, 50, WHITE);
 
         EndDrawing();
 	}
